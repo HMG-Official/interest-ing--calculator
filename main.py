f7a12bfd0bc0b0cc7655b97ff10d434977c0cf13
Rathinagiri S <srathinagiri@gmail.com>
2024-04-13 17:33:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Interest(ing) Calculator", layout="wide", initial_sidebar_state="auto")
+
+st.write( "#  Hearty Welcome to Interest(ing) Calculator")
