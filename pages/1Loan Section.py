5d62603e53ee0d29ad55c8e0f618911c7cd5823c
Rathinagiri S <srathinagiri@gmail.com>
2024-04-14 10:05:40 +0530

    Update 1Loan Section.py

--- pages/1Loan Section.py
+++ pages/1Loan Section.py
@@ -386,13 +386,13 @@ with col1:
     st.selectbox("Compounding",("Monthly", "Quarterly","Half Yearly","Yearly"), help="Select Interest Compounding Period", label_visibility="collapsed", key="loan_comp")
     st.button("Instalment Frequency", disabled=True, use_container_width=True)
     st.selectbox("Frequency",("Months", "Quarters","Half Years","Years"), label_visibility="collapsed", key="loan_freq")
-    st.button("Principal", on_click=loan_prin_calc, help="Click to calculate Principal after entering all the other values.", use_container_width=True)
+    st.button("Principal", on_click=loan_prin_calc, help="Click to calculate Loan Principal after entering all the other values.", use_container_width=True)
     st.number_input("Principal", min_value=0.0, step=1000.0, label_visibility="collapsed", key="loan_prin")
-    st.button("Interest Rate (% p.a.)", on_click=loan_rate_calc, use_container_width=True)
+    st.button("Interest Rate (% p.a.)", on_click=loan_rate_calc, help="Click to calculate Loan Interest Rate after entering all the other values.",use_container_width=True)
     st.number_input("Interest Rate", min_value=0.0, step=0.5, label_visibility="collapsed", key="loan_rate")
-    st.button("Term", on_click=loan_term_calc, use_container_width=True)
+    st.button("Term", on_click=loan_term_calc, help="Click to calculate Number of Instalments after entering all the other values.", use_container_width=True)
     st.number_input("Term", min_value=0.0,  step=1.0, label_visibility="collapsed", key="loan_term")
-    st.button("Instalment", on_click=loan_inst_calc, use_container_width=True)
+    st.button("Instalment", on_click=loan_inst_calc, help="Click to calculate Instalment amount after entering all the other values.", use_container_width=True)
     st.number_input("Instalment", min_value=0.0, step=1000.0, label_visibility="collapsed", key="loan_inst")
 
 df = st.session_state.loan_df
