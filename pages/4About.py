0d3648f0a7c4df99bb7942f18b7217534d11c02e
Rathinagiri S <srathinagiri@gmail.com>
2024-04-14 10:18:43 +0530

    Update 4About.py

--- pages/4About.py
+++ pages/4About.py
@@ -2,20 +2,20 @@ import streamlit as st
 
 # About page content
 st.write("# About :Red[Interest(ing) Calculator]")
-st.write("Welcome to Interest(ing) Calculator - a web app developed by :rainbow[Rathinagiri Subbiah].")
+st.write("Welcome to Interest(ing) Calculator - a free, open source web app for calculating Loan, Deposit variables.")
 
 st.subheader("Developer Information")
-st.write("Developer: :blue[Rathinagiri Subbiah]")
+st.write("Developer: :rainbow[Rathinagiri Subbiah]")
 st.write("Email: srathinagiri@gmail.com")
 
 st.subheader("Tools Used")
-st.write("This web app was built using :blue[Python] and :blue[Streamlit].")
+st.write("This web app was built using :violet[Python] and :violet[Streamlit].")
 
 st.subheader("Release Date")
 st.write("Release Date: 2024-04-14")
 
 st.subheader("Assumption")
-st.write("For calculations, it is assumed that each month consists of :blue[30] days.")
+st.write("For calculations, it is assumed that each month consists of :violet[30] days.")
 
 st.subheader("Disclaimer")
 st.write("This web app is for informational purposes only. Please seek professional advice for financial decisions.")
