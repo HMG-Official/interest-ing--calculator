78d6f9cadf2301e10193ded97dbfa572a57699a5
Rathinagiri S <srathinagiri@gmail.com>
2024-04-14 10:20:28 +0530

    Update 4About.py

--- pages/4About.py
+++ pages/4About.py
@@ -9,13 +9,13 @@ st.write("Developer: :rainbow[Rathinagiri Subbiah]")
 st.write("Email: srathinagiri@gmail.com")
 
 st.subheader("Tools Used")
-st.write("This web app was built using :violet[Python] and :violet[Streamlit].")
+st.write("This web app was built using :violet[**Python**] and :violet[**Streamlit**].")
 
 st.subheader("Release Date")
 st.write("Release Date: 2024-04-14")
 
 st.subheader("Assumption")
-st.write("For calculations, it is assumed that each month consists of :violet[30] days.")
+st.write("For calculations, it is assumed that each month consists of :violet[**30**] days.")
 
 st.subheader("Disclaimer")
 st.write("This web app is for informational purposes only. Please seek professional advice for financial decisions.")
