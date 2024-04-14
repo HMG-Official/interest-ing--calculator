f7cce0961d98eb31a8e36e1e75cc5f5b3f7fd3df
Rathinagiri S <srathinagiri@gmail.com>
2024-04-14 08:55:09 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,3 +3,4 @@ import streamlit as st
 st.set_page_config(page_title="Interest(ing) Calculator", layout="wide", initial_sidebar_state="auto")
 
 st.write( "# :pray: Hearty Welcome to Interest(ing) Calculator")
+st.image("IntCalc.jpg")
